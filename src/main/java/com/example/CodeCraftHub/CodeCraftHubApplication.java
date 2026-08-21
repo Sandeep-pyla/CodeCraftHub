b7a92c79150cc901c5a833f7da6c91aa46a445d5
Sandeep-pyla <152224121+Sandeep-pyla@users.noreply.github.com>
2026-08-20 20:37:57 -0500

    Add files via upload

---
+++ src/main/java/com/example/CodeCraftHub/CodeCraftHubApplication.java
@@ -0,0 +1,13 @@
+package com.example.CodeCraftHub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeCraftHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeCraftHubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/CodeCraftHub/CodeCraftHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CodeCraftHub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeCraftHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
